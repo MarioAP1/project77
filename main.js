44f01a9adee2535b44e90d45c38f8842cccf1602
MarioAP1 <105328243+MarioAP1@users.noreply.github.com>
2022-08-25 19:32:25 -0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,15 @@
+
+var first_name = "Mario";
+
+var last_name = "Perez";
+
+var a = 10;
+
+var b = 5;
+
+console.log(last_name)
+
+first_name.concat(last_name);
+
+var add = a + b ;
+console.log(add);
